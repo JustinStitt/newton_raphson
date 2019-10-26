4d0e552fe579f25ed6484e8662a3732ba83a5f37
Justin Stitt <cpt.crixus@gmail.com>
2019-10-26 15:37:36 -0700

    fixed up newtons_method implementation

--- Newton-Raphson_Method/root-finding.cpp
+++ Newton-Raphson_Method/root-finding.cpp
@@ -3,7 +3,7 @@
 * also known as the Newton–Raphson method, 
 * named after Isaac Newton and Joseph Raphson, 
 * is a root-finding algorithm which produces 
-* successively better approximations to the roots of a real-valued function.
+* successively better approximations to the roots of a real-valued function. - wiki
 */
 
 //TO:DO's... make a parser to read a poly_func from user input
@@ -13,13 +13,13 @@
 #include <cmath>
 #include <vector>
 
-const int DECIMAL_PRECISION = 7;
+const int DECIMAL_PRECISION = 15;//how many decimal points used for formatting ONLY. doesnt effect actual math procedures (iomanip)
 
 using namespace std;
 
 double newtons_method(double, int);
 
-struct monomial {
+struct monomial {//a monomial is a (coeff,power) couple. i.e; (3x^2)... coeff = 3, power = 2
 	double coeff;
 	double power;
 };
@@ -31,8 +31,10 @@ public:
 	vector<monomial> terms;//a vector of type struct monomial. this forms a polynomial function ex) f(x) = 3x^2 + 4x^1 + 1x^0 
 	poly_function()//default constructor
 	{
+		//not much needs to be done as far as constructors go...
+		//we are appending all our necessary data POST construction.
 	}
-	void add_term(struct monomial to_add)
+	void add_term(struct monomial to_add)//appends a new monomial to the BACK of our poly_function
 	{
 		terms.push_back(to_add);
 	}
@@ -46,11 +48,14 @@ public:
 		}
 		return running_total;
 	}
-	void display(int index = -1)
+	void display( bool is_der = false, int index = -1)//prints entire poly_func or just a single term if index arg is changed. (index >= 0)
 	{
 		if (index == -1)//by default, print entire polynomial function
 		{
-			cout << "f(x) = ";
+			if (!is_der)
+				cout << "f(x) = ";
+			else//if our func has the derivative tag, then add the ' to f(x) -> f'(x)
+				cout << "f'(x) = ";
 			for (int i = 0; i < terms.size(); i++)
 			{
 				if (i == 0 && terms[i].coeff >= 0)//if we are on the first index, dont print the positive sign
@@ -97,36 +102,43 @@ poly_function some_func, some_der;
 
 int main()
 {
-	
-	monomial some_term,some_term2,some_term3;
+	monomial some_term,some_term2,some_term3;//a monomial is a (coeff,power) pairing... i.e; (3x^2) is coeff = 3, power = 2
+
+	//We could make a parser to get user input, but for now... manually input each term before runtime.
+	//TERM 0
 	some_term.coeff = 1.00;
 	some_term.power = 3;
+	//TERM 1
 	some_term2.coeff = -2.00;
 	some_term2.power = 1;
+	//TERM 2
 	some_term3.coeff = -5.00;
 	some_term3.power = 0;
-	some_func.add_term(some_term);
-	some_func.add_term(some_term2);
-	some_func.add_term(some_term3);
+	//ADDING TERMS TO OUR POLYNOMIAL FUNCTION
+	some_func.add_term(some_term);//0
+	some_func.add_term(some_term2);//1
+	some_func.add_term(some_term3);//2
 
-	some_der.terms = some_func.derivative();
-	some_func.display();
-	some_der.display();
-	cout << "f(5) = " << some_func.at(5) << endl;
+	some_der.terms = some_func.derivative();//calculating the derivative and setting our derivative function to hold its new terms
 
-	cout << "100 iterations of newtons_method of f(x) returns: " << fixed <<setprecision(DECIMAL_PRECISION) <<  newtons_method(2,100) << endl;
+	some_func.display();//print out poly func
+	some_der.display(true);//print out poly func der
 
-	system("PAUSE>nul");
+
+	cout << "100 iterations of newtons_method of f(x) returns: " << fixed <<setprecision(DECIMAL_PRECISION) <<  newtons_method(2.0,100) << endl;
+
+	system("PAUSE>nul");//windows only. stops the cmd console from insta-closing
 	return 0;
 }
 
 
-double newtons_method(double x, int iterations = 10)//default iterations set to 10
+double newtons_method(double x, int iterations)//recursive function to iteratively estimate roots of any given function using its derivative and a starting term
 {
 	double val = x - (some_func.at(x) / some_der.at(x));
-	if(iterations > 0)//if we still have some iterations remaining then recursively call newtons_method with our newly calculated 'val'
+	if (iterations > 0)//if we still have some iterations to go
 	{
-		newtons_method(val, --iterations);
+		val = newtons_method(val, --iterations);//the new value of val is equal to the subsequent newtons approximation of val.
 	}
-	return val;
+	return val;//after our iterations are done. output the final value of val
+
 }
